7d61b2c5e9fd50721fb94b69227468cdbd22a2c4
Roger Retita <roger.retita@gmail.com>
2025-09-05 21:54:24 +0200

    cta section

---
+++ frontend/src/components/shared/CTA/cta.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './cta.css';
+import ctaImage from './images/cta.png';
+
+const Cta = () => {
+
+  return (
+    <div className="cta-container">
+
+      <div className='box'>
+
+        <div className="cta-content">
+            <p className='title'>Lorem ipsom<br/> doloresd sit amet</p>
+            <p className='subtitle'>cuisinées avec amour</p> 
+            <button className='btn-primary scrolled'>Nous rejoindres</button>
+        </div>
+
+        <div className="cta-image">
+            <img src={ctaImage} alt="image de cuisinière" />
+        </div>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default Cta;
--- frontend/src/pages/client/HomePage.jsx
+++ frontend/src/pages/client/HomePage.jsx
@@ -3,12 +3,14 @@ import Hero from '../../components/shared/Hero/Hero.jsx';
 import Apropos from '../../components/shared/About/About.jsx';   
 import NosPlats from '../../components/shared/Nosplat/Nosplat.jsx';
 import CommentCaMarche from '../../components/shared/CommentCaMarche/CommentCaMarche.jsx';
+import Cta from '../../components/shared/CTA/cta.jsx';
 
 export default function HomePage() {
   return <>
     <Hero />
     <Apropos />
     <NosPlats />
-    <CommentCaMarche /> 
+    <CommentCaMarche />
+    <Cta />
   </>;
 }
