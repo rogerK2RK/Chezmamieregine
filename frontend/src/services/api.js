b1175346d62348b101f18259a15aef96a2f7ee4f
Roger Retita <roger.retita@gmail.com>
2025-10-07 23:46:55 +0200

    apischange

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,8 @@
 // frontend/src/services/api.js
 import axios from "axios";
 
-const BASE = import.meta.env.VITE_API_URL ?? "http://localhost:5000";
-
 const api = axios.create({
-  baseURL: BASE + "/api",
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
