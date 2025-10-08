082587b9519dd84c47a96871cb97b5e241124488
Roger Retita <roger.retita@gmail.com>
2025-10-08 21:30:32 +0200

    about mob

--- frontend/src/components/shared/About/About.jsx
+++ frontend/src/components/shared/About/About.jsx
@@ -2,35 +2,37 @@ import './About.css';
 import imageapropos from "./images/Apropos.png";
 
 export default function ImageTextSlider() {
-
   return (
-    <div className="section-container">
-      <div className="section-image">
-        <img
-          src={imageapropos}
-          alt="Illustration"
-        />
-      </div>
+    <div className="section-container-all">
+      <h2>À Propos</h2>
+      <div className="section-container">
+        <div className="section-image">
+          <img
+            src={imageapropos}
+            alt="Illustration"
+          />
+        </div>
 
-      {/* Texte droite */}
-      <div className="section-text">
-        <h2>À Propos</h2>
-        <p>
-          Découvrez nos plats faits maison, préparés avec passion et livrés
-          directement chez vous. Une cuisine authentique, inspirée des saveurs
-          malgaches, pour partager des moments uniques.
-        </p>
-        <p>
-          Découvrez nos plats faits maison, préparés avec passion et livrés
-          directement chez vous. Une cuisine authentique, inspirée des saveurs
-          malgaches, pour partager des moments uniques.
-        </p>
-        <p>
-          Découvrez nos plats faits maison, préparés avec passion et livrés
-          directement chez vous. Une cuisine authentique, inspirée des saveurs
-          malgaches, pour partager des moments uniques.
-        </p>
-        <button className="btn-primary">Découvrir le menu</button>
+        {/* Texte droite */}
+        <div className="section-text">
+          
+          <p>
+            Découvrez nos plats faits maison, préparés avec passion et livrés
+            directement chez vous. Une cuisine authentique, inspirée des saveurs
+            malgaches, pour partager des moments uniques.
+          </p>
+          <p>
+            Découvrez nos plats faits maison, préparés avec passion et livrés
+            directement chez vous. Une cuisine authentique, inspirée des saveurs
+            malgaches, pour partager des moments uniques.
+          </p>
+          <p>
+            Découvrez nos plats faits maison, préparés avec passion et livrés
+            directement chez vous. Une cuisine authentique, inspirée des saveurs
+            malgaches, pour partager des moments uniques.
+          </p>
+          <button className="btn-primary">Découvrir le menu</button>
+        </div>
       </div>
     </div>
   );
