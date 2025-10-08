45509bd9f7189e64559db035f43267b5f723bf5c
Roger Retita <roger.retita@gmail.com>
2025-10-08 22:19:52 +0200

    cta mob

--- frontend/src/components/shared/CTA/cta.jsx
+++ frontend/src/components/shared/CTA/cta.jsx
@@ -3,24 +3,21 @@ import './cta.css';
 import ctaImage from './images/cta.png';
 
 const Cta = () => {
-
   return (
     <div className="cta-container">
-
-      <div className='box'>
-
+      <div className="box">
         <div className="cta-content">
-            <p className='title'>Lorem ipsom<br/> doloresd sit amet</p>
-            <p className='subtitle'>cuisinées avec amour</p> 
-            <button className='btn-primary scrolled'>Nous rejoindres</button>
+          <p className="title">
+            Lorem ipsom<br /> doloresd sit amet
+          </p>
+          <p className="subtitle">cuisinées avec amour</p>
+          <button className="btn-primary scrolled">Nous rejoindres</button>
         </div>
 
         <div className="cta-image">
-            <img src={ctaImage} alt="image de cuisinière" />
+          <img src={ctaImage} alt="image de cuisinière" />
         </div>
-
       </div>
-
     </div>
   );
 };
